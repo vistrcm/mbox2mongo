9835f35e36895897f7cbf8da803ebaa7b733c238
Stanislav Vitko <svitko@griddynamics.com>
2017-02-20 02:13:23 -0800

    parallel version without deadlocks

--- mongobody2words.py
+++ mongobody2words.py
@@ -4,6 +4,9 @@ from __future__ import print_function
 
 import argparse
 import string
+from multiprocessing import Process
+from multiprocessing import Queue
+from multiprocessing import cpu_count
 
 import html2text
 from pymongo import MongoClient
@@ -34,19 +37,42 @@ def remove_unprintable(input_body):
     return text
 
 
-def worker(item):
-    processed_text = process_body(item["body"])
-    return processed_text.split()
+def worker(url, db, collection, task_queue, done_queue):
+    worker_col = MongoClient(url)[db][collection]
+    for item_id in iter(task_queue.get, 'STOP'):
+        item = worker_col.find_one({"_id": item_id})
+        processed_text = process_body(item["body"])
+        result = processed_text.split()
+        done_queue.put(result)
 
 
 def process_records(url, db, collection):
-    col = MongoClient(url)[db][collection]
+    col = MongoClient(url, connect=False)[db][collection]
 
-    items = col.find()
-    for item in items:
-        processed_text = process_body(item["body"])
-        for word in processed_text.split():
-            print(word)
+    ids = []
+
+    for i in col.find({}, projection={'_id': True}).limit(1000):
+        ids.append(i["_id"])
+
+    task_queue = Queue()
+    done_queue = Queue()
+    # Submit tasks
+    for task in ids:
+        task_queue.put(task)
+
+    # Start worker processes
+    for i in range(cpu_count()):
+        Process(target=worker, args=(url, db, collection, task_queue, done_queue)).start()
+
+    # Get and print results
+    print('Unordered results:')
+    for _ in range(len(ids)):
+        for item in done_queue.get():
+            print(item)
+
+    # Tell child processes to stop
+    for i in range(cpu_count()):
+        task_queue.put('STOP')
 
 
 if __name__ == '__main__':
