bf7787bc687e489774b50091da4ef2c1fb2919ad
Stanislav Vitko <svitko@griddynamics.com>
2017-02-19 18:06:56 -0800

    update cleanupbody.py to pring single words

--- cleanupbody.py
+++ cleanupbody.py
@@ -33,19 +33,7 @@ def process_body(input_body):
     # leave only printable symbols
     text = remove_unprintable(input_body)
 
-    # try to decode from base64.
-    # NOTE: may be wrong with some probability
-    text = urlsafe_b64decode_if_necessary(text)
-
-    # leave only printable symbols
-    text = remove_unprintable(text)
-
-    # decode MIME quoted-printable data
-    text = quopri.decodestring(text)
-
-    # converts a HTML into ASCII text
-    text_in_utf = text.decode('latin-1')
-    plain = html2text.html2text(text_in_utf)  # convert bytes to string
+    plain = html2text.html2text(text)  # convert bytes to string
     return plain
 
 
@@ -57,33 +45,12 @@ def remove_unprintable(input_body):
 def process_records(url, db, collection):
     col = MongoClient(url)[db][collection]
     for doc in col.find():
-        print(">" * 100)
-        print(process_body(doc["body"]))
-        print("<" * 100)
-
-
-def urlsafe_b64decode_if_necessary(s):
-    try:
-        # try to urlsafe_b64decode and decode resulting bytes to latin-1
-        bytes_decoded = base64.urlsafe_b64decode(s)
-        string_decoded = bytes_decoded.decode('latin-1')
-        return string_decoded
-    except binascii.Error:
-        return s
+        processed_text = process_body(doc["body"])
+        for word in processed_text.split():
+            print(word)
 
 if __name__ == '__main__':
     args = parse_args()
     process_records(args.mongo_url, args.mongo_db, args.mongo_collection)
 
-    # record = get_record(args.mongo_url, args.mongo_db, args.mongo_collection)
-    # print(record["body"])
-    # processed = process_body(record["body"])
-    # print(processed)
-
-    # from sklearn.feature_extraction.text import CountVectorizer
-    # count_vect = CountVectorizer()
-    # X_train_counts = count_vect.fit_transform([processed])
-    # print(X_train_counts.shape)
-    # print(X_train_counts)
-    # print(count_vect.get_stop_words())
 
