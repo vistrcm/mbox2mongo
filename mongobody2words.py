fa037d59c6fd0e53686dc2a211ee7f4b380747aa
Stanislav Vitko <svitko@griddynamics.com>
2017-02-21 23:25:18 -0800

    filter most of non-letter words

--- mongobody2words.py
+++ mongobody2words.py
@@ -24,15 +24,32 @@ logger.addHandler(ch)
 # Prepare translation table to clean some non-important symbols.
 # See http://www.unicode.org/reports/tr44/#General_Category_Values for unicode categories
 exclude_unicode_set = {
-    'C',  # Other:	Cc | Cf | Cs | Co | Cn
-    'Zl',  # Line_Separator:	U+2028 LINE SEPARATOR only
-    'Zp'  # Paragraph_Separator:	U+2029 PARAGRAPH SEPARATOR only
+    "Cc",  # Control: a C0 or C1 control code
+    "Cf",  # Format: a format control character
+    "Cs",  # Surrogate: a surrogate code point
+    "Co",  # Private_Use: a private-use character
+    "Cn",  # Unassigned: a reserved unassigned code point or a noncharacter
+    "Zl",  # Line_Separator:	U+2028 LINE SEPARATOR only
+    "Zp",  # Paragraph_Separator:	U+2029 PARAGRAPH SEPARATOR only
+    "Sm",  # Math_Symbol: a symbol of mathematical use
+    "Sc",  # Currency_Symbol: a currency sign
+    "Sk",  # Modifier_Symbol: a non-letterlike modifier symbol
+    "So",  # Other_Symbol: a symbol of other type
+    "Pc",  # Connector_Punctuation: a connecting punctuation mark, like a tie
+    "Pd",  # Dash_Punctuation: a dash or hyphen punctuation mark
+    "Ps",  # Open_Punctuation: an opening punctuation mark (of a pair)
+    "Pe",  # Close_Punctuation: a closing punctuation mark (of a pair)
+    "Pi",  # Initial_Punctuation: an initial quotation mark
+    "Pf",  # Final_Punctuation: a final quotation mark
+    "Po",  # Other_Punctuation: a punctuation mark of other type
 }
 
-include_symbols = '@'  # keep this symbols
+include_symbols = '@./?'  # "@" and "." for emails; "/" and "?" for urls (only path) glue
 all_chars = set(chr(i) for i in range(sys.maxunicode))
-exclude_chars = set(c for c in all_chars if unicodedata.category(c) in exclude_unicode_set and c not in include_symbols)
+exclude_chars = set(c for c in all_chars if unicodedata.category(c) in exclude_unicode_set )
 translate_table = {ord(character): " " for character in exclude_chars}
+# replace "@" and "." and others by ``None`` to glue emails and urls
+translate_table.update({ord(character): None for character in include_symbols})
 
 
 def parse_args():
