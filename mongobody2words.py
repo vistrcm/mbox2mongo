a9db30accee2b00f2b182b65154b37e48f329eae
Stanislav Vitko <svitko@griddynamics.com>
2017-03-12 15:08:59 -0700

    print and write processed items to mongo coll

--- mongobody2words.py
+++ mongobody2words.py
@@ -2,14 +2,14 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
+
 import argparse
 import logging
-import sys
 from email import utils
 from multiprocessing import Process
 from multiprocessing import Queue
 from multiprocessing import cpu_count
-
 from pymongo import MongoClient
 
 from utils import text_to_words
@@ -30,17 +30,19 @@ def parse_args():
     )
     parser.add_argument("mongo_url", help="mongodb url")
     parser.add_argument("--mongo_db", "-d", help="mongo database name", default="archive")
-    parser.add_argument("--mongo_collection", "-c", help="mongo collection name", default="mails")
+    parser.add_argument("--mongo_read_collection", "-c", help="mongo read collection name", default="mails")
+    parser.add_argument("--mongo_write_collection", "-o", help="mongo write collection name", default="mails_proc")
     parser.add_argument("--workers", "-w", help="number of workers", default=cpu_count(), type=int)
     return parser.parse_args()
 
 
-def worker(url, db, collection, task_queue, done_queue):
+def worker(url, db, collection, task_queue, done_queue, write_queue):
     worker_col = MongoClient(url)[db][collection]
     for item_id in iter(task_queue.get, 'STOP'):
         item = worker_col.find_one({"_id": item_id})
-        words = process_item(item)["body_words"]
-        for word in words:
+        processed_item = process_item(item)
+        write_queue.put(processed_item)
+        for word in processed_item["body_words"]:
             done_queue.put(word)
 
 
@@ -49,8 +51,8 @@ def process_item(item):
     from_raw = item["headers"]["from"].lower()  # lower everything to simplify
     from_name, from_email = utils.parseaddr(from_raw)
 
-    to_emails = emails_from_header(item["headers"]["to"].lower())  # lower everything to simplify
-    cc_emails = emails_from_header(item["headers"]["cc"].lower())  # lower everything to simplify
+    to_emails = emails_from_header(item["headers"].get("to", "").lower())  # lower everything to simplify
+    cc_emails = emails_from_header(item["headers"].get("cc", "").lower())  # lower everything to simplify
 
     # get words
     words = text_to_words(item["body"])
@@ -68,7 +70,7 @@ def process_item(item):
 
 def emails_from_header(raw_header):
     addresses = utils.getaddresses([raw_header])  # raw_header folded in list to make getaddresses work
-    emails = [item(1) for item in addresses]  # skip name and use only email address
+    emails = [item[1] for item in addresses]  # skip name and use only email address
     return emails
 
 
@@ -81,21 +83,32 @@ def print_worker(done_queue):
             pass
 
 
-def process_records(url, db, collection, num_workers=1):
-    col = MongoClient(url)[db][collection]
+def write_worker(url, db, write_col, write_queue):
+    writer_col = MongoClient(url)[db][write_col]
+    for item in iter(write_queue.get, 'STOP_WRITING'):
+        write_id = writer_col.insert_one(item).inserted_id
+
+
+def process_records(url, db, read_collection, write_collection, num_workers=1):
+    col = MongoClient(url)[db][read_collection]
     cursor = col.find({}, projection={'_id': True})
 
     task_queue = Queue()
     print_queue = Queue()
+    write_queue = Queue()
 
     # Print worker
     print_process = Process(target=print_worker, args=(print_queue,))
     print_process.start()
 
+    # write worker
+    write_process = Process(target=write_worker, args=(url, db, write_collection, write_queue,))
+    write_process.start()
+
     # Start worker processes
     worker_processes = []
     for i in range(num_workers):
-        p = Process(target=worker, args=(url, db, collection, task_queue, print_queue))
+        p = Process(target=worker, args=(url, db, read_collection, task_queue, print_queue, write_queue))
         p.start()
         worker_processes.append(p)
 
@@ -114,7 +127,12 @@ def process_records(url, db, collection, num_workers=1):
     for p in worker_processes:
         p.join()
 
-    logger.info("stopping printer via 'STOP' in queue")
+    logger.info("stopping writer via 'STOP_WRITING' in queue")
+    print_queue.put('STOP_WRITING')
+    logger.info("joining writer")
+    write_process.join()
+
+    logger.info("stopping printer via 'STOP_PRINTING' in queue")
     print_queue.put('STOP_PRINTING')
     logger.info("joining printer")
     print_process.join()
@@ -122,4 +140,10 @@ def process_records(url, db, collection, num_workers=1):
 
 if __name__ == '__main__':
     args = parse_args()
-    process_records(args.mongo_url, args.mongo_db, args.mongo_collection, args.workers)
+    process_records(
+        args.mongo_url,
+        args.mongo_db,
+        args.mongo_read_collection,
+        args.mongo_write_collection,
+        args.workers
+    )
